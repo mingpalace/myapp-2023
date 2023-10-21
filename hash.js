7780fd82bb1677283205e1d3b75a06a4bbcc9eb8
Minginswe <xiaomingvlog@gmail.com>
2023-10-21 09:42:20 +0200

    daw

--- hash.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require("bcryptjs");
-const saltRounds = 10;
-
-bcrypt.genSalt(saltRounds, function (err, salt) {
-  bcrypt.hash(plainPassword, salt, function (err, hash) {
-    console.log(hash);
-  });
-});
